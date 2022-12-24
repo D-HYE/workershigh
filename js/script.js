5fcd2274d5567d10e09d4f4d0a0e8dce6bbd8b39
D-HYE <34275823+D-HYE@users.noreply.github.com>
2022-12-25 00:51:15 +0900

    first commit 1225

--- js/script.js
+++ js/script.js
@@ -9,21 +9,29 @@ $(function(){
         }else{
             $(this).parents('.select_wrap').addClass('active');
         }
-    })
+    });
     $('.select_wrap .sel_option').click(function(){
         let option_val = $(this).text();
-        $(this).closest('.select_wrap').find('.selected_value').text(option_val)
+        $(this).closest('.select_wrap').find('.selected_value').text(option_val);
         $(this).closest('.select_wrap').toggleClass('active');
     })
 
     $('.date_wrap').on('click', function(){
         $(this).toggleClass('active');
-        $('.calander_area').toggle()
+        $('.calander_area').toggle();
     })
 
     $('#file_upload').on('change',function(){
         let file_name = $('#file_upload').val();
         $(".upload_val").text(file_name);
     });
-    
+
+    $('.popup .close_btn').on('click', function(){
+        $('.popup').removeClass('on');
+        $('.dim').removeClass('on');
+    });
+    $('.popup .cancle_btn').on('click', function(){
+        $('.popup').removeClass('on');
+        $('.dim').removeClass('on');
+    });
 })
